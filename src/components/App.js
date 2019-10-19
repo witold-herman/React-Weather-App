753b9d56e7c5c93e721f00f4222999260cd51b5a
witold-herman <56408790+witold-herman@users.noreply.github.com>
2019-10-19 17:43:30 +0200

    Patched Components App.js and Result.js
    Solved CORS problems, for now it works well.
    The function of displaying weather data for a given city has been added.

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Form from './Form'
 import Result from './Result';
 
+// Key for weather API
+const APIKey = 'a2c2e25fb273a23265e497499e8edbda';
+
 class App extends React.Component {
 
     state = {
@@ -13,7 +16,7 @@ class App extends React.Component {
         temp: '',
         pressure: '',
         wind: '',
-        err: '',
+        err: false,
     };
 
     handleInputChange = (e) => {
@@ -25,11 +28,42 @@ class App extends React.Component {
     handleCitySubmit = e => {
         e.preventDefault();
 
-        const API = `http://api.openweathermap.org/data/2.5/find?q=${this.state.value}&APPID=a2c2e25fb273a23265e497499e8edbda`;
+        const API = `http://api.openweathermap.org/data/2.5/find?q=${this.state.value}&APPID=${APIKey}&units=metric`;
 
-        fetch(API, {method: 'get', headers: new Headers({'Access-Control-Allow-Origin': '*'})})
-            .then(response => console.log(response))
-            .catch(err => console.log(err))
+        fetch(API/*{method: 'get', headers: new Headers({'Access-Control-Allow-Origin': '*'})}*/)
+            .then(response => {
+                if (response.ok) {
+                    return response
+                }
+                throw Error('Nie udało się')
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.list[0].main.temp !== undefined) {
+                    const time = new Date().toLocaleString();
+                    this.setState(prevState => ({
+                        err: false,
+                        date: time,
+                        city: 'Pogoda dla miasta ' + this.state.value,
+                        //      sunrise: data.sys.sunrise,
+                        //     sunset: data.sys.sunset,
+                        temp: 'Temperatura: ' + data.list[0].main.temp,
+                        pressure: 'Ciśnienie: ' +  data.list[0].main.pressure,
+                        wind: 'Prędkość wiatru: ' + data.list[0].wind.speed,
+                    }))
+                }
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState(state => ({
+                    err: true,
+                    city: 'Nie odnaleziono miasta!',
+                    date: '',
+                    temp: '',
+                    pressure: '',
+                    wind: '',
+                }))
+            })
     };
 
     render() {
@@ -40,7 +74,7 @@ class App extends React.Component {
                     change={this.handleInputChange}
                     submit={this.handleCitySubmit}
                 />
-                <Result/>
+                <Result weather={this.state}/>
             </div>
         )
     }
