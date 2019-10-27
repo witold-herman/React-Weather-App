35168d1b42b8873b51d94423e796db122b6e6b93
witold-herman <56408790+witold-herman@users.noreply.github.com>
2019-10-27 14:19:50 +0100

    Maybe final version of components
    -added css stylesheets
    -patched the app.js code
    -patched the result.js code

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Form from './Form'
 import Result from './Result';
+import './App.css'
 
 // Key for weather API
 const APIKey = 'a2c2e25fb273a23265e497499e8edbda';
@@ -25,46 +26,88 @@ class App extends React.Component {
         })
     };
 
-    handleCitySubmit = e => {
-        e.preventDefault();
+    // handleCitySubmit = e => {
+    //     e.preventDefault();
+    //
+    //     const API = `http://api.openweathermap.org/data/2.5/find?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+    //
+    //     fetch(API/*{method: 'get', headers: new Headers({'Access-Control-Allow-Origin': '*'})}*/)
+    //         .then(response => {
+    //             if (response.ok) {
+    //                 return response
+    //             }
+    //             throw Error('Nie udało się')
+    //         })
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             if (data.list[0].main.temp !== undefined) {
+    //                 const time = new Date().toLocaleString();
+    //                 this.setState(prevState => ({
+    //                     err: false,
+    //                     date: time,
+    //                     city:this.state.value,
+    //                     //      sunrise: data.sys.sunrise,
+    //                     //     sunset: data.sys.sunset,
+    //                     temp: data.list[0].main.temp,
+    //                     pressure:data.list[0].main.pressure,
+    //                     wind: data.list[0].wind.speed,
+    //                 }))
+    //             }
+    //         })
+    //         .catch(err => {
+    //             console.log(err);
+    //             this.setState(state => ({
+    //                 err: true,
+    //                 city: state.value.charAt(0).toUpperCase() + state.value.slice(1),
+    //                 date: '',
+    //                 temp: '',
+    //                 pressure: '',
+    //                 wind: '',
+    //             }))
+    //         })
+    // };
 
-        const API = `http://api.openweathermap.org/data/2.5/find?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(this.state.value.length === 0) return;
+        if (prevState.value !== this.state.value) {
+                const API = `http://api.openweathermap.org/data/2.5/find?q=${this.state.value}&APPID=${APIKey}&units=metric`;
 
-        fetch(API/*{method: 'get', headers: new Headers({'Access-Control-Allow-Origin': '*'})}*/)
-            .then(response => {
-                if (response.ok) {
-                    return response
-                }
-                throw Error('Nie udało się')
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.list[0].main.temp !== undefined) {
-                    const time = new Date().toLocaleString();
-                    this.setState(prevState => ({
-                        err: false,
-                        date: time,
-                        city: 'Pogoda dla miasta ' + this.state.value,
-                        //      sunrise: data.sys.sunrise,
-                        //     sunset: data.sys.sunset,
-                        temp: 'Temperatura: ' + data.list[0].main.temp,
-                        pressure: 'Ciśnienie: ' +  data.list[0].main.pressure,
-                        wind: 'Prędkość wiatru: ' + data.list[0].wind.speed,
-                    }))
-                }
-            })
-            .catch(err => {
-                console.log(err);
-                this.setState(state => ({
-                    err: true,
-                    city: 'Nie odnaleziono miasta!',
-                    date: '',
-                    temp: '',
-                    pressure: '',
-                    wind: '',
-                }))
-            })
-    };
+                fetch(API/*{method: 'get', headers: new Headers({'Access-Control-Allow-Origin': '*'})}*/)
+                    .then(response => {
+                        if (response.ok) {
+                            return response
+                        }
+                        throw Error('Nie udało się')
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.list[0].main.temp !== undefined) {
+                            const time = new Date().toLocaleString();
+                            this.setState(prevState => ({
+                                err: false,
+                                date: time,
+                                city:this.state.value,
+                                //      sunrise: data.sys.sunrise,
+                                //     sunset: data.sys.sunset,
+                                temp: data.list[0].main.temp,
+                                pressure:data.list[0].main.pressure,
+                                wind: data.list[0].wind.speed,
+                            }))
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        this.setState(state => ({
+                            err: true,
+                            city: state.value.charAt(0).toUpperCase() + state.value.slice(1),
+                            date: '',
+                            temp: '',
+                            pressure: '',
+                            wind: '',
+                        }))
+                    })
+        }
+    }
 
     render() {
         return (
@@ -72,7 +115,6 @@ class App extends React.Component {
                 <Form
                     value={this.state.value}
                     change={this.handleInputChange}
-                    submit={this.handleCitySubmit}
                 />
                 <Result weather={this.state}/>
             </div>
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import './Result.css';
 
 const Result = props => {
     const{err, city, temp, date, pressure, wind} = props.weather;
+
+    let content = null;
+
+    if(!err && city) {
+        content = (
+            <div>
+                <h3>Wyniki wyszukiwania dla <em>{city}</em></h3>
+                <h4>Dane dla dnia i godziny: {date}</h4>
+                <h4>Aktualna temperatura: {temp} &#176;C</h4>
+                <h4>Aktualne ciśnienie: {pressure} hPa</h4>
+                <h4>Aktualna siła wiatru: {wind} m/s</h4>
+            </div>
+        )
+    }
+
     return (
-        <React.Fragment>
-            <div>{city}</div>
-            <div>{date}</div>
-            <div>{temp}</div>
-            <div>{wind}</div>
-            <div>{pressure}</div>
-        </React.Fragment>
+        <div className="result">
+            {err ? `Nie odnaleziono ${city}` : content}
+        </div>
+
+
 
     )
 };
